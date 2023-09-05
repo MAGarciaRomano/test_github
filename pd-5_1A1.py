f652998568f773191afdf1144b34b429d0eac0c7
MAGarciaRomano <105550751+MAGarciaRomano@users.noreply.github.com>
2023-09-04 21:27:03 -0300

    Delete pd-5_1A1.py

--- pd-5_1A1.py
+++
@@ -1,8 +0,0 @@
-
-# Alumno: Forkear el siguiente repositorio de Github de inoveAlumnos:
-# https://github.com/InoveAlumnos/test_github
-
-# Crear una variable llamada "mi_repositorio" y en ella
-# almacene la URL o link de su repositorio forkeado
-# ejemplo: 
-#  mi_repositorio = https://github.com/MAGarciaRomano/test_github.git
