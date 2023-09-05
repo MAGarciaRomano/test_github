63c54395aba4f665f08f27475b93503153ffe41a
MAGarciaRomano <105550751+MAGarciaRomano@users.noreply.github.com>
2023-09-04 21:17:51 -0300

    Add files via upload

---
+++ pd-5_1A1.py
@@ -0,0 +1,8 @@
+
+# Alumno: Forkear el siguiente repositorio de Github de inoveAlumnos:
+# https://github.com/InoveAlumnos/test_github
+
+# Crear una variable llamada "mi_repositorio" y en ella
+# almacene la URL o link de su repositorio forkeado
+# ejemplo: 
+#  mi_repositorio = https://github.com/MAGarciaRomano/test_github.git
